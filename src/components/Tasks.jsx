e2658df9be60d3b04ee8841fe7e6ec84b6e36b8e
Anderson <andersonamalta@gmail.com>
2025-09-17 21:06:00 -0300

    feat: ensure tasks are listed on Tasks components

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -5,8 +5,18 @@ import SunIcon from "../assets/icons/sun.svg?react"
 import CloudSunIcon from "../assets/icons/cloud-sun.svg?react"
 import MoonIcon from "../assets/icons/moon.svg?react"
 import TaskSeparator from "./TasksSeparator"
+import { useState } from "react"
+import TASKS from "../constants/tasks"
+import TaskIem from "./TaskItem"
 
 const Tasks = () => {
+  const [tasks, setTasks] = useState(TASKS)
+
+  // Pega todas as tarefas com o time igual a morning e armazena nessa variavel
+  const morningTasks = tasks.filter((task) => task.time == "Manhã")
+  const aftrnoonTasks = tasks.filter((task) => task.time == "Tarde")
+  const eveningTasks = tasks.filter((task) => task.time == "Noite")
+
   return (
     <div className="w-full px-8 py-16">
       <div className="flex w-full justify-between">
@@ -35,16 +45,31 @@ const Tasks = () => {
         {/* Manhã */}
         <div className="space-y-3">
           <TaskSeparator title="Manhã" icon={<SunIcon />} />
+
+          {/* Lista todas as tarefas */}
+          {morningTasks.map((task) => (
+            <TaskIem key={task.id} task={task} />
+          ))}
         </div>
 
         {/* Tarde */}
         <div className="my-6 space-y-3">
           <TaskSeparator title="Tarde" icon={<CloudSunIcon />} />
+
+          {/* Lista todas as tarefas */}
+          {aftrnoonTasks.map((task) => (
+            <TaskIem key={task.id} task={task} />
+          ))}
         </div>
 
         {/* Noite */}
         <div className="space-y-3">
           <TaskSeparator title="Noite" icon={<MoonIcon />} />
+
+          {/* Lista todas as tarefas */}
+          {eveningTasks.map((task) => (
+            <TaskIem key={task.id} task={task} />
+          ))}
         </div>
       </div>
     </div>
