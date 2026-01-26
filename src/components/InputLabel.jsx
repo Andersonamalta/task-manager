b0618ce5b18ff49837c2d80e6160c8952b968f88
Anderson <andersonamalta@gmail.com>
2026-01-26 19:09:37 -0300

    chore: add prop-types to InputLabel components

--- src/components/InputLabel.jsx
+++ src/components/InputLabel.jsx
@@ -1,9 +1,15 @@
+import PropTypes from "prop-types"
+
 const InputLabel = (props) => {
   return (
     <label className="text-sm font-semibold text-[#35383E]" {...props}>
       {props.children}
     </label>
   )
+}
+
+InputLabel.prototype = {
+  children: PropTypes.node.isRequired,
 }
 
 export default InputLabel
