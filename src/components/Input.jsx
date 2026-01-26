a474ce73be2e5be3f614b3226b3249a09c56e3bf
Anderson <andersonamalta@gmail.com>
2026-01-26 19:06:25 -0300

    chore: add prop-types to Input components

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,6 @@
 import InputLabel from "./InputLabel"
 import InputErrorMessage from "./InputErrorMessage"
+import PropTypes from "prop-types"
 
 const Input = ({ label, error, ...rest }) => {
   return (
@@ -7,27 +8,17 @@ const Input = ({ label, error, ...rest }) => {
       <InputLabel htmlFor={rest.id}>{label}</InputLabel>
       <input
         {...rest}
-        className="rounded-lg border border-solid border-[#ECECEC] px-4 py-3 placeholder-[#9A9C9F] placeholder:text-sm focus:outline-none focus:ring-2 focus:ring-[#00ADB5]"
+        className="rounded-lg border border-solid border-[#ECECEC] px-4 py-3 placeholder-[#9A9C9F] placeholder:text-sm focus:ring-2 focus:ring-[#00ADB5] focus:outline-none"
       />
       {error && <InputErrorMessage>{error.message}</InputErrorMessage>}
     </div>
   )
 }
 
-/*const Input = forwardRef(({ label, error, ...rest }, ref) => {
-  return (
-    <div className="flex flex-col space-y-1 text-left">
-      <InputLabel htmlFor={rest.id}>{label}</InputLabel>
-      <input
-        {...rest}
-        ref={ref}
-        className="rounded-lg border border-solid border-[#ECECEC] px-4 py-3 placeholder-[#9A9C9F] placeholder:text-sm focus:outline-none focus:ring-2 focus:ring-[#00ADB5]"
-      />
-      {error && (
-        <span className="text-left text-xs text-red-500">{error.message}</span>
-      )}
-    </div>
-  )
-})*/
+Input.prototype = {
+  label: PropTypes.string.isRequired,
+  InputErrorMessage: PropTypes.string,
+  id: PropTypes.string.isRequired,
+}
 
 export default Input
