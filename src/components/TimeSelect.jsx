bec8e8f53622939e97d0bd4941ae5769854745cf
Anderson <andersonamalta@gmail.com>
2026-01-26 19:26:40 -0300

    chore: add prop-types to TimeSelect components

--- src/components/TimeSelect.jsx
+++ src/components/TimeSelect.jsx
@@ -1,5 +1,6 @@
 import InputLabel from "./InputLabel"
 import InputErrorMessage from "./InputErrorMessage"
+import PropTypes from "prop-types"
 
 const TimeSelect = (props) => {
   return (
@@ -7,7 +8,7 @@ const TimeSelect = (props) => {
       <InputLabel htmlfor="time">Horário</InputLabel>
       <select
         id="time"
-        className="appearance-none rounded-lg border border-solid border-[#ECECEC] px-4 py-3 placeholder-[#9A9C9F] placeholder:text-sm focus:outline-none focus:ring-2 focus:ring-[#00ADB5]"
+        className="appearance-none rounded-lg border border-solid border-[#ECECEC] px-4 py-3 placeholder-[#9A9C9F] placeholder:text-sm focus:ring-2 focus:ring-[#00ADB5] focus:outline-none"
         {...props}
       >
         <option value="Manhã">Manhã</option>
@@ -17,6 +18,10 @@ const TimeSelect = (props) => {
       {props.error && <InputErrorMessage>{error.message}</InputErrorMessage>}
     </div>
   )
+}
+
+TimeSelect.prototype = {
+  InputErrorMessage: PropTypes.string,
 }
 
 export default TimeSelect
