4047d9ef2bd3cb39c79019c142bbf4eba0928b3e
Anderson <andersonamalta@gmail.com>
2026-02-16 14:12:23 -0300

    feat: ensure SidebarButton changes according to active route

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -16,12 +16,12 @@ const Sidebar = () => {
       </div>
 
       <div className="flex flex-col gap-2 p-2">
-        <SidebarButton href="/" color="unselected">
+        <SidebarButton to="/">
           <HomeIcon />
           Início
         </SidebarButton>
 
-        <SidebarButton href="/tasks" color="selected">
+        <SidebarButton to="/tasks">
           {" "}
           <TaskIcon /> Minhas Tarefas
         </SidebarButton>
